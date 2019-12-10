3386f9d47e459ec9e9d5891b015cff19df1341ec
James Smith <jgsmith@gmail.com>
2019-12-10 11:24:41 -0500

    Add specs and clean up IntCode

--- lib/advent_of_code/int_code.ex
+++ lib/advent_of_code/int_code.ex
@@ -1,4 +1,6 @@
 defmodule AdventOfCode.IntCode do
+  alias AdventOfCode.IntCode
+
   defstruct rom: {},
             ram: %{},
             pc: 0,
@@ -6,44 +8,72 @@ defmodule AdventOfCode.IntCode do
             stdin: [],
             stdout: []
 
+  @type t :: %IntCode{
+          rom: tuple,
+          ram: map,
+          pc: integer,
+          rp: integer,
+          stdin: [integer],
+          stdout: [integer]
+        }
+
+  @type mode :: 0 | 1 | 2
+
+  @spec new(tuple, [integer]) :: t
   def new(rom, stdin \\ []) do
     %__MODULE__{rom: rom, stdin: stdin}
   end
 
   @doc """
   ## Examples
+
     iex> IntCode.run(IntCode.new({1002,4,3,4,33}))
     {:done, {[1002,4,3,4,99], []}}
+
     iex> IntCode.run(IntCode.new({3,0,4,0,99}, [123]))
     {:done, {[123,0,4,0,99], [123]}}
+
     iex> IntCode.run(IntCode.new({1101,100,-1,4,0}))
     {:done, {[1101,100,-1,4,99], []}}
+
     iex> IntCode.run(IntCode.new({3,9,8,9,10,9,4,9,99,-1,8}, [8]))
     {:done, {[3,9,8,9,10,9,4,9,99,1,8], [1]}}
+
     iex> IntCode.run(IntCode.new({3,9,8,9,10,9,4,9,99,-1,8}, [10]))
     {:done, {[3,9,8,9,10,9,4,9,99,0,8], [0]}}
+
     iex> IntCode.run(IntCode.new({3,3,1108,-1,8,3,4,3,99}, [8]))
     {:done, {[3,3,1108,1,8,3,4,3,99], [1]}}
+
     iex> IntCode.run(IntCode.new({3,3,1108,-1,8,3,4,3,99}, [10]))
     {:done, {[3,3,1108,0,8,3,4,3,99], [0]}}
+
     iex> IntCode.run(IntCode.new({104,1125899906842624,99}))
     {:done, {[104,1125899906842624,99], [1125899906842624]}}
+
     iex> IntCode.run(IntCode.new({1102,34915192,34915192,7,4,7,99,0}))
     {:done, {[1102,34915192,34915192,7,4,7,99,1219070632396864], [1219070632396864]}}
+
     iex> IntCode.run(IntCode.new({109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99}))
-    {:done, {[109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99], [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]}}
+    {:done, {[109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 1], [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]}}
+
     iex> IntCode.run(IntCode.new({3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
     ...> 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
     ...> 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99}, [7]))
     {:done, {[3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
       1106,0,36,98,0,7,1002,21,125,20,4,20,1105,1,46,104,
       999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [999]}}
+
     iex> IntCode.run(IntCode.new({3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
     ...> 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
     ...> 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99}, [8]))
     {:done, {[3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
       1106,0,36,98,1000,8,1002,21,125,20,4,20,1105,1,46,104,
       999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [1000]}}
+
     iex> IntCode.run(IntCode.new({3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
     ...> 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
     ...> 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99}, [9]))
@@ -51,6 +81,7 @@ defmodule AdventOfCode.IntCode do
       1106,0,36,98,1001,9,1002,21,125,20,4,20,1105,1,46,104,
       999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [1001]}}
   """
+  @spec run(t) :: t | {:done, {[integer], [integer]}} | {:wait, t}
   def run(machine) do
     case execute(machine, parsed(at(machine, pc(machine), 1))) do
       :done -> {:done, output_memory(machine)}
@@ -59,8 +90,10 @@ defmodule AdventOfCode.IntCode do
     end
   end
 
+  @spec execute(t, {integer, integer, integer, integer}) :: t | :done | :wait
   def execute(_, {99, _, _, _}), do: :done
 
+  # add two numbers
   def execute(machine, {1, mode1, mode2, mode3}) do
     result = at(machine, pc(machine) + 1, mode1) + at(machine, pc(machine) + 2, mode2)
 
@@ -69,6 +102,7 @@ defmodule AdventOfCode.IntCode do
     |> advance(:pc, 4)
   end
 
+  # multiply two numbers
   def execute(machine, {2, mode1, mode2, mode3}) do
     result = at(machine, pc(machine) + 1, mode1) * at(machine, pc(machine) + 2, mode2)
 
@@ -77,6 +111,7 @@ defmodule AdventOfCode.IntCode do
     |> advance(:pc, 4)
   end
 
+  # pull a number from input - suspend if nothing in input
   def execute(machine, {3, mode1, _, _}) do
     case read(machine) do
       :wait ->
@@ -89,28 +124,32 @@ defmodule AdventOfCode.IntCode do
     end
   end
 
+  # push a number to output
   def execute(machine, {4, mode1, _, _}) do
     value = at(machine, pc(machine) + 1, mode1)
-    # IO.inspect({:output, value})
+
     machine
     |> write(value)
     |> advance(:pc, 2)
   end
 
+  # jump if value is non-zero
   def execute(machine, {5, mode1, mode2, _}) do
     case at(machine, pc(machine) + 1, mode1) do
       0 -> advance(machine, :pc, 3)
-      _ -> set(machine, :pc, at(machine, pc(machine) + 2, mode2))
+      _ -> jump_to(machine, at(machine, pc(machine) + 2, mode2))
     end
   end
 
+  # jump if value is zero
   def execute(machine, {6, mode1, mode2, _}) do
     case at(machine, pc(machine) + 1, mode1) do
-      0 -> set(machine, :pc, at(machine, pc(machine) + 2, mode2))
+      0 -> jump_to(machine, at(machine, pc(machine) + 2, mode2))
       _ -> advance(machine, :pc, 3)
     end
   end
 
+  # store 1 if first value is less than second value, 0 otherwise
   def execute(machine, {7, mode1, mode2, mode3}) do
     result =
       if at(machine, pc(machine) + 1, mode1) < at(machine, pc(machine) + 2, mode2),
@@ -122,6 +161,7 @@ defmodule AdventOfCode.IntCode do
     |> advance(:pc, 4)
   end
 
+  # store 1 if first value is equal to second value, 0 otherwise
   def execute(machine, {8, mode1, mode2, mode3}) do
     result =
       if at(machine, pc(machine) + 1, mode1) == at(machine, pc(machine) + 2, mode2),
@@ -133,6 +173,7 @@ defmodule AdventOfCode.IntCode do
     |> advance(:pc, 4)
   end
 
+  # add value to relative pointer
   def execute(machine, {9, mode1, _, _}) do
     machine
     |> advance(:rp, at(machine, pc(machine) + 1, mode1))
@@ -141,11 +182,13 @@ defmodule AdventOfCode.IntCode do
 
   @doc """
   Returns a tuple with the opcode and address modes for the given instruction.
+
   ## Examples
+
     iex> IntCode.parsed(1002)
     {2, 0, 1, 0}
   """
-  @spec parsed(term) :: {term, term, term, term}
+  @spec parsed(integer) :: {integer, integer, integer, integer}
   def parsed(instruction) do
     {
       # opcode
@@ -159,9 +202,13 @@ defmodule AdventOfCode.IntCode do
     }
   end
 
+  @spec pc(t) :: integer
   def pc(%{pc: value}), do: value
+
+  @spec rp(t) :: integer
   def rp(%{rp: value}), do: value
 
+  @spec advance(t, :pc | :rp, integer) :: t
   def advance(%{pc: value} = machine, :pc, offset) do
     %{machine | pc: value + offset}
   end
@@ -170,32 +217,57 @@ defmodule AdventOfCode.IntCode do
     %{machine | rp: value + offset}
   end
 
-  def set(machine, :pc, value), do: %{machine | pc: value}
+  @spec jump_to(t, integer) :: t
+  def jump_to(machine, value), do: %{machine | pc: value}
+
+  @doc """
+  Adds the value to the machine's output.
+  """
+  @spec write(t, integer) :: t
   def write(%{stdout: stdout} = machine, value), do: %{machine | stdout: [value | stdout]}
 
+  @doc """
+  Reads a value from the machine's input. If no values are available, the machine
+  will suspend.
+  """
+  @spec read(t) :: t | :wait
   def read(%{stdin: []} = machine), do: :wait
   def read(%{stdin: [value | stdin]} = machine), do: {value, %{machine | stdin: stdin}}
 
+  @doc """
+  Adds a value to the end of the input. Useful if the machine is stopped waiting
+  on anothe value.
+  """
+  @spec provide(t, integer) :: t
   def provide(%{stdin: stdin} = machine, value), do: %{machine | stdin: stdin ++ [value]}
 
+  @doc """
+  Gets the last value output by the machine.
+  """
+  @spec retrieve(t) :: integer
   def retrieve(%{stdout: [value | _]}), do: value
-  def retrieve(_), do: nil
 
+  @spec output_memory(t) :: {[integer], [integer]}
   defp output_memory(%{rom: rom, ram: ram, stdout: stdout} = machine) do
     top = max_addr(rom, ram)
+
     readout = for i <- 0..top, do: at(machine, i, 1)
     {readout, Enum.reverse(stdout)}
   end
 
-  defp max_addr(rom, %{}), do: tuple_size(rom) - 1
-
+  @spec max_addr(tuple, map) :: integer
   defp max_addr(rom, ram) do
-    Enum.max([
-      tuple_size(rom),
-      Enum.max(Map.keys(ram)) + 1
-    ]) - 1
+    if map_size(ram) == 0 do
+      tuple_size(rom) - 1
+    else
+      Enum.max([
+        tuple_size(rom),
+        Enum.max(Map.keys(ram)) + 1
+      ]) - 1
+    end
   end
 
+  @spec store(t, integer, mode, integer) :: t
   defp store(machine, addr, 0, value) do
     store(machine, at(machine, addr, 1), 1, value)
   end
@@ -208,6 +280,7 @@ defmodule AdventOfCode.IntCode do
     store(machine, at(machine, addr, 1) + rp, 1, value)
   end
 
+  @spec at(t, integer, mode) :: integer
   defp at(machine, addr, 0), do: at(machine, at(machine, addr, 1), 1)
 
   defp at(%{rom: rom, ram: ram} = _machine, addr, 1) do
